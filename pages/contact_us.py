7657320089b3fbad77303a4706109a380bc54ced
ikhumwa edmond mulera <edmondmulera55@gmail.com>
2024-03-17 08:44:19 +0300

    create input email,text area and submit button

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -3,5 +3,11 @@ import streamlit as st
 st.header("contact me")
 
 
+with st.form(key="my form"):
+    useer_email = st.text_input("Your Email Address")
+    message = st.text_area("your Message ")
+    button = st.form_submit_button()
+
+
 
 
